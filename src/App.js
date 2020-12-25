11fc64e66c3edb009e1ed2b9d0b7a8765e308e8e
Mashekwa Sikatema <msikatema@gmail.com>
2020-12-25 22:21:28 +0000

    Added book lists and css styles

--- src/App.js
+++ src/App.js
@@ -5,8 +5,34 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-        <h1>My book collection</h1>
-        <img src={profile_img} alt="PROFILE IMAGE" />
+        <h1 className="title">My book collection</h1>
+        <img className="profile-pic" src={profile_img} alt="profile-pic"/>
+      
+        <ol className="list">
+        <p>Books I've read</p>
+          <li>
+            <a href="http://www.google.com/search?q=The+Da+Vinci+Code">The Da Vinci Code</a>
+          </li>
+          <li>
+            <a href="http://www.google.com/search?q=Digital+Fortress">Digital Fortress</a>
+          </li>
+          <li>
+            <a href="http://www.google.com/search?q=Thus+spoke+Zarathustra">Thus spoke Zarathustra</a>
+          </li>
+          <li>
+          <a href="http://www.google.com/search?q=Frankenstein">Frankenstein</a>
+          </li>
+          <li>
+          <a href="http://www.google.com/search?q=Sherlock+Holmes">Sherlock Holmes</a>
+          </li>
+        </ol>
+        
+        <ul className="list">
+          <p>Currently reading</p>
+          <li>
+          <a href="http://www.google.com/search?q=Ultimate+Hitchhiker%27s+guide+to+the+galaxy">Ultimate Hitchhiker's guide to the galaxy</a>
+          </li>
+        </ul>
       </div>
     )
   }
